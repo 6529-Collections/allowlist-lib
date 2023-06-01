686dd16b5d826ec661fa14aa79a29a6654ac2998
Simo <info@analyticsflow.ee>
2023-06-01 12:45:39 +0300

    feat: added descriptions to operations

--- src/utils/allowlist-operation-code.utils.ts
+++ src/utils/allowlist-operation-code.utils.ts
@@ -34,6 +34,75 @@ export const ALLOWLIST_OPERATION_CODE_TO_TYPE: Record<
     AllowlistOperationType.ITEM,
 };
 
+export const ALLOWLIST_CODE_DESCRIPTIONS: Record<
+  AllowlistOperationCode,
+  {
+    title: string;
+    description: string;
+  }
+> = {
+  [AllowlistOperationCode.CREATE_ALLOWLIST]: {
+    title: 'Create Allowlist',
+    description: 'Create a new allowlist',
+  },
+  [AllowlistOperationCode.GET_COLLECTION_TRANSFERS]: {
+    title: 'Get Collection Transfers',
+    description: 'Get all transfers for a collection',
+  },
+  [AllowlistOperationCode.CREATE_TOKEN_POOL]: {
+    title: 'Create Token Pool',
+    description: 'Create a new token pool',
+  },
+  [AllowlistOperationCode.CREATE_CUSTOM_TOKEN_POOL]: {
+    title: 'Create Custom Token Pool',
+    description: 'Create a new custom token pool',
+  },
+  [AllowlistOperationCode.CREATE_WALLET_POOL]: {
+    title: 'Create Wallet Pool',
+    description: 'Create a new wallet pool',
+  },
+  [AllowlistOperationCode.ADD_PHASE]: {
+    title: 'Add Phase',
+    description: 'Add a new phase to a allowlist',
+  },
+  [AllowlistOperationCode.ADD_COMPONENT]: {
+    title: 'Add Component',
+    description: 'Add a new component to a phase',
+  },
+  [AllowlistOperationCode.COMPONENT_ADD_SPOTS_TO_ALL_ITEM_WALLETS]: {
+    title: 'Add Spots to All Item Wallets',
+    description: 'Add spots to all item wallets in a component',
+  },
+  [AllowlistOperationCode.ADD_ITEM]: {
+    title: 'Add Item',
+    description: 'Add a new item to a component',
+  },
+  [AllowlistOperationCode.ITEM_EXCLUE_TOKEN_IDS]: {
+    title: 'Exclude Token IDs',
+    description: 'Exclude token IDs from an item',
+  },
+  [AllowlistOperationCode.ITEM_SELECT_TOKEN_IDS]: {
+    title: 'Select Token IDs',
+    description: 'Select token IDs from an item',
+  },
+  [AllowlistOperationCode.ITEM_REMOVE_FIRST_N_TOKENS]: {
+    title: 'Remove First N Tokens',
+    description: 'Remove first N tokens from an item',
+  },
+  [AllowlistOperationCode.ITEM_REMOVE_LAST_N_TOKENS]: {
+    title: 'Remove Last N Tokens',
+    description: 'Remove last N tokens from an item',
+  },
+  [AllowlistOperationCode.ITEM_SELECT_FIRST_N_TOKENS]: {
+    title: 'Select First N Tokens',
+    description: 'Select first N tokens from an item',
+  },
+  [AllowlistOperationCode.ITEM_SELECT_LAST_N_TOKENS]: {
+    title: 'Select Last N Tokens',
+    description: 'Select last N tokens from an item',
+  },
+};
+
 export const getCodesForType = (type: AllowlistOperationType) => {
   if (!Object.values(AllowlistOperationType).includes(type)) {
     throw new BadInputError(`Invalid AllowlistOperationType: ${type}`);

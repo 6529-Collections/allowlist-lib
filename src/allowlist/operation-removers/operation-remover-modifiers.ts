084100fcad8a87e83360e0252d346524e5f05683
tarmo <tarmokalling@gmail.com>
2023-11-17 15:00:05 +0200

    Changed an interface to type
    Signed-off-by: tarmo <tarmokalling@gmail.com>

--- src/allowlist/operation-removers/operation-remover-modifiers.ts
+++ src/allowlist/operation-removers/operation-remover-modifiers.ts
@@ -16,9 +16,9 @@ export interface OperationModifierResponse<T extends AllowlistOperation> {
   readonly id: string | null;
 }
 
-export interface OperationModifier<T extends AllowlistOperation> {
+export type OperationModifier<T extends AllowlistOperation> = {
   (params: OperationModifierParams<T>): OperationModifierResponse<T>;
-}
+};
 
 export const defaultModifier = <T extends AllowlistOperation>({
   targetIds,

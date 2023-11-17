d0a5715c42813801d2dfe63fe19c9fa84f70f227
tarmo <tarmokalling@gmail.com>
2023-11-17 16:49:50 +0200

    refactored a validator
    Signed-off-by: tarmo <tarmokalling@gmail.com>

--- src/allowlist/operations/create-custom-token-pool/create-custom-token-pool-operation.ts
+++ src/allowlist/operations/create-custom-token-pool/create-custom-token-pool-operation.ts
@@ -1,7 +1,7 @@
-import { AllowlistOperationExecutor } from '../../../allowlist/allowlist-operation-executor';
-import { AllowlistState } from '../../..//allowlist/state-types/allowlist-state';
+import { AllowlistOperationExecutor } from '../../allowlist-operation-executor';
+import { AllowlistState } from '../../state-types/allowlist-state';
 
-import { BadInputError } from '../../..//allowlist/bad-input.error';
+import { BadInputError } from '../../bad-input.error';
 import { isEthereumAddress } from '../../../utils/validators';
 import { CustomTokenPoolParams } from './create-custom-token-pool-operation.types';
 
@@ -9,42 +9,49 @@ export class CreateCustomTokenPoolOperation
   implements AllowlistOperationExecutor
 {
   validate(params: any): params is CustomTokenPoolParams {
-    if (!params.hasOwnProperty('id')) {
-      throw new BadInputError('Missing id');
+    this.assertIdValid(params);
+    this.assertNameValid(params);
+    this.assertDescriptionValid(params);
+    this.assertTokenBaseValid(params);
+
+    for (const token of params.tokens) {
+      this.assertTokenIdValid(token);
+      this.assertTokenOwnersValid(token);
+    }
+    return true;
+  }
+
+  private assertTokenOwnersValid(token) {
+    if (!token.hasOwnProperty('owner')) {
+      throw new BadInputError('Invalid tokens');
     }
 
-    if (typeof params.id !== 'string') {
-      throw new BadInputError('Invalid id');
+    if (typeof token.owner !== 'string') {
+      throw new BadInputError('Invalid tokens');
     }
 
-    if (!params.id.length) {
-      throw new BadInputError('Invalid id');
+    if (!token.owner.length) {
+      throw new BadInputError('Invalid tokens');
     }
 
-    if (!params.hasOwnProperty('name')) {
-      throw new BadInputError('Missing name');
+    if (!isEthereumAddress(token.owner)) {
+      throw new BadInputError('Invalid tokens');
     }
+  }
 
-    if (typeof params.name !== 'string') {
-      throw new BadInputError('Invalid name');
+  private assertTokenIdValid(token) {
+    if (token.hasOwnProperty('id')) {
+      if (typeof token.id !== 'string') {
+        throw new BadInputError('Invalid tokens');
+      }
+
+      if (!token.id.length) {
+        throw new BadInputError('Invalid tokens');
+      }
     }
+  }
 
-    if (!params.name.length) {
-      throw new BadInputError('Invalid name');
-    }
-
-    if (!params.hasOwnProperty('description')) {
-      throw new BadInputError('Missing description');
-    }
-
-    if (typeof params.description !== 'string') {
-      throw new BadInputError('Invalid description');
-    }
-
-    if (!params.description.length) {
-      throw new BadInputError('Invalid description');
-    }
-
+  private assertTokenBaseValid(params: any) {
     if (!params.hasOwnProperty('tokens')) {
       throw new BadInputError('Missing tokens');
     }
@@ -67,36 +74,48 @@ export class CreateCustomTokenPoolOperation
     if (haveTokenId && haveNotTokenId) {
       throw new BadInputError('Invalid tokens');
     }
+  }
 
-    for (const token of params.tokens) {
-      if (token.hasOwnProperty('id')) {
-        if (typeof token.id !== 'string') {
-          throw new BadInputError('Invalid tokens');
-        }
-
-        if (!token.id.length) {
-          throw new BadInputError('Invalid tokens');
-        }
-      }
-
-      if (!token.hasOwnProperty('owner')) {
-        throw new BadInputError('Invalid tokens');
-      }
-
-      if (typeof token.owner !== 'string') {
-        throw new BadInputError('Invalid tokens');
-      }
-
-      if (!token.owner.length) {
-        throw new BadInputError('Invalid tokens');
-      }
-
-      if (!isEthereumAddress(token.owner)) {
-        throw new BadInputError('Invalid tokens');
-      }
+  private assertDescriptionValid(params: any) {
+    if (!params.hasOwnProperty('description')) {
+      throw new BadInputError('Missing description');
     }
 
-    return true;
+    if (typeof params.description !== 'string') {
+      throw new BadInputError('Invalid description');
+    }
+
+    if (!params.description.length) {
+      throw new BadInputError('Invalid description');
+    }
+  }
+
+  private assertNameValid(params: any) {
+    if (!params.hasOwnProperty('name')) {
+      throw new BadInputError('Missing name');
+    }
+
+    if (typeof params.name !== 'string') {
+      throw new BadInputError('Invalid name');
+    }
+
+    if (!params.name.length) {
+      throw new BadInputError('Invalid name');
+    }
+  }
+
+  private assertIdValid(params: any) {
+    if (!params.hasOwnProperty('id')) {
+      throw new BadInputError('Missing id');
+    }
+
+    if (typeof params.id !== 'string') {
+      throw new BadInputError('Invalid id');
+    }
+
+    if (!params.id.length) {
+      throw new BadInputError('Invalid id');
+    }
   }
 
   execute({

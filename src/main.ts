4adc9ed31b935585aa290c6a148255c6517ac61a
GelatoGenesis <tarmokalling@gmail.com>
2026-03-04 11:10:15 +0200

    arweave fallback
    Signed-off-by: GelatoGenesis <tarmokalling@gmail.com>

--- src/main.ts
+++
@@ -1,8 +0,0 @@
-import { SeizeApi } from './services/seize/seize.api';
-import { Http } from './services/http';
-import { ConsoleLogListener, LoggerFactory } from './logging/logging-emitter';
-
-new SeizeApi(
-  new Http(new LoggerFactory(new ConsoleLogListener())),
-  'https://api.6529.io/api',
-).getDataForBlock({ path: '/consolidated_uploads', blockId: 24580372 });
